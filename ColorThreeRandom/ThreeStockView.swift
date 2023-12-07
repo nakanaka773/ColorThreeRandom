711f6c58e43529c3f49dbb093bbb13b390029310
nakanaka773 <hirayama773@icloud.com>
2023-12-08 07:20:42 +0900

    リリース用

---
+++ ColorThreeRandom/ThreeStockView.swift
@@ -0,0 +1,18 @@
+//
+//  ThreeStockView.swift
+//  ColorThreeRandom
+//
+//  Created by 平山奈々海 on 2023/12/08.
+//
+
+import SwiftUI
+
+struct ThreeStockView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ThreeStockView()
+}
