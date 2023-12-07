3173aa853696db9a4ce35a5b3a793d78cdf12157
nakanaka773 <hirayama773@icloud.com>
2023-12-08 03:05:09 +0900

    DataViewに切り替えた

---
+++ ColorThreeRandom/Data.swift
@@ -0,0 +1,8 @@
+//
+//  Data.swift
+//  ColorThreeRandom
+//
+//  Created by 平山奈々海 on 2023/12/08.
+//
+
+import Foundation
---
+++ ColorThreeRandom/DataView.swift
@@ -0,0 +1,18 @@
+//
+//  DataView.swift
+//  ColorThreeRandom
+//
+//  Created by 平山奈々海 on 2023/12/08.
+//
+
+import SwiftUI
+
+struct DataView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    DataView()
+}
